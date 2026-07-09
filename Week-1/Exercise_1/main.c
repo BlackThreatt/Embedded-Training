9211b6e05db5793dd7f2f77e2bc8abea6f24edbb
Mohamed Ali Bessaidi <bessaidimohamedali99@gmail.com>
2026-07-09 04:41:53 +0100

    feat: Create custom memset function

--- Week-1/Exercise_1/main.c
+++ Week-1/Exercise_1/main.c
@@ -2,16 +2,29 @@
 #include <stdio.h>
 
 void *mem_cpy(void *dest, const void *src, uint32_t size);
+void *mem_set(void *s, int c, size_t n);
 
 int main(void) {
   char *a = "Test\n";
   char *b;
 
   mem_cpy(&b, &a, 6);
-
   printf("%s\n", b);
 
+  mem_set(&b, 68, 2);
+  printf("%s\n", a);
+
   return 0;
+}
+
+void *mem_set(void *s, int c, size_t n) {
+  unsigned char *s_ptr = s;
+  unsigned char t = (unsigned char)c;
+
+  for (uint32_t iter = 0; iter < n; iter++) {
+    s_ptr[iter] = t;
+  }
+  return s;
 }
 
 void *mem_cpy(void *dest, const void *src, uint32_t size) {
