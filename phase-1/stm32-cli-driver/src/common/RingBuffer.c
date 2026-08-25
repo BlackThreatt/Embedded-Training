c5bc99fb82d848f2e0aec5cfee92fe0ae9f9801a
Mohamed Ali Bessaidi <bessaidimohamedali99@gmail.com>
2026-08-25 12:44:38 +0100

    refactor(RingBuffer): Integrate Doxygen template to files

--- phase-1/stm32-cli-driver/src/common/RingBuffer.c
+++ phase-1/stm32-cli-driver/src/common/RingBuffer.c
@@ -1,10 +1,49 @@
+/**
+ ******************************************************************************
+ * @file           : RingBuffer.h
+ * @brief          : Header file for RingBuffer.c
+ * @author         : Mohamed Ali BESSAIDI
+ * @date           : 25 Aug 2026
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 MOHAMED ALI BESSAIDI.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
 #include "RingBuffer.h"
 
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+/**
+ * @brief Initalizes ring buffer structure to null.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval None
+ */
 void RingBuf_Init(RingBuffer_t *rb) {
   rb->head = 0;
   rb->tail = 0;
 }
 
+/**
+ * @brief Writes byte element to a given ring buffer.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @param[in] element Byte to be pushed to ring buffer.
+ * @retval true of success, false otherwise
+ */
 bool RingBuf_Push(RingBuffer_t *rb, uint8_t element) {
   uint32_t head = rb->head;
   if ((head - rb->tail) >= RING_BUF_SIZE) {
@@ -15,6 +54,12 @@ bool RingBuf_Push(RingBuffer_t *rb, uint8_t element) {
   return true;
 }
 
+/**
+ * @brief Reads byte element from a given ring buffer.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @param[out] element Pointer to byte that will store popped value.
+ * @retval true if success, false otherwise
+ */
 bool RingBuf_Pop(RingBuffer_t *rb, uint8_t *element) {
   uint32_t tail = rb->tail;
   if (rb->head == tail) {
@@ -24,14 +69,26 @@ bool RingBuf_Pop(RingBuffer_t *rb, uint8_t *element) {
   rb->tail = tail + 1U;
   return true;
 }
-uint32_t RingBuf_Count(const RingBuffer_t *rb) {
-  return rb->head - rb->tail; 
-}
 
-bool RingBuf_IsEmpty(const RingBuffer_t *rb) {
-  return rb->head == rb->tail; 
-}
+/**
+ * @brief Reads the size of a given ring buffer.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval size of ring buffer
+ */
+uint32_t RingBuf_Count(const RingBuffer_t *rb) { return rb->head - rb->tail; }
 
+/**
+ * @brief Checks whether ring buffer is empty.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval true if empty, false otherwise
+ */
+bool RingBuf_IsEmpty(const RingBuffer_t *rb) { return rb->head == rb->tail; }
+
+/**
+ * @brief Checks whether ring buffer is full.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval true if full, false otherwise
+ */
 bool RingBuf_IsFull(const RingBuffer_t *rb) {
   return (rb->head - rb->tail) >= RING_BUF_SIZE;
 }
--- phase-1/stm32-cli-driver/src/common/RingBuffer.h
+++ phase-1/stm32-cli-driver/src/common/RingBuffer.h
@@ -1,26 +1,96 @@
-#ifndef RING_BUFFER_H
-#define RING_BUFFER_H
+/**
+ ******************************************************************************
+ * @file           : RingBuffer.h
+ * @brief          : Header file for RingBuffer.c
+ * @author         : Mohamed Ali BESSAIDI
+ * @date           : 25 Aug 2026
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 MOHAMED ALI BESSAIDI.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
+#ifndef RINGBUFFER_H
+#define RINGBUFFER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
 #include <stdbool.h>
 #include <stdint.h>
 
+/* Exported constants --------------------------------------------------------*/
 #define RING_BUF_SIZE (256U) /* must be power of 2 */
 #define RING_BUF_MASK (RING_BUF_SIZE - 1U)
 
 _Static_assert((RING_BUF_SIZE & RING_BUF_MASK) == 0u,
                "RING_BUF_SIZE must be a power of two");
 
+/* Exported macros -----------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+/**
+ * @brief  RingBuffer configuration structure
+ */
 typedef struct {
-  volatile uint8_t buf[RING_BUF_SIZE];
-  volatile uint32_t head;
-  volatile uint32_t tail;
+  volatile uint8_t
+      buf[RING_BUF_SIZE]; /*!< Buffer array that stores ring buffer data */
+  volatile uint32_t head; /*!< Ringbuffer head index */
+  volatile uint32_t tail; /*!< Ring buffer tail index */
 } RingBuffer_t;
 
+/* Exported functions prototypes ---------------------------------------------*/
+
+/**
+ * @brief Initalizes ring buffer structure to null.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval None
+ */
 void RingBuf_Init(RingBuffer_t *rb);
+
+/**
+ * @brief Writes byte element to a given ring buffer.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @param[in] element Byte to be pushed to ring buffer.
+ * @retval true of success, false otherwise
+ */
 bool RingBuf_Push(RingBuffer_t *rb, uint8_t element);
+
+/**
+ * @brief Reads byte element from a given ring buffer.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @param[out] element Pointer to byte that will store popped value.
+ * @retval true if success, false otherwise
+ */
 bool RingBuf_Pop(RingBuffer_t *rb, uint8_t *element);
+
+/**
+ * @brief Reads the size of a given ring buffer.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval size of ring buffer
+ */
 uint32_t RingBuf_Count(const RingBuffer_t *rb);
+
+/**
+ * @brief Checks whether ring buffer is empty.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval true if empty, false otherwise
+ */
 bool RingBuf_IsEmpty(const RingBuffer_t *rb);
+
+/**
+ * @brief Checks whether ring buffer is full.
+ * @param[in] rb Pointer to RingBuffer_t configuration structure.
+ * @retval true if full, false otherwise
+ */
 bool RingBuf_IsFull(const RingBuffer_t *rb);
 
-#endif /* RING_BUFFER_H */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RINGBUFFER_H */
